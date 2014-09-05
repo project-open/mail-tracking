a4696dd046d3983bb446b4d2b44c7ae8b2cb1185
Frank Bergmann <frank.bergmann@project-open.com>
2014-09-05 11:49:24 +0000

    - Mail Tracking:   Disable callback, because it creates strange errors

--- tcl/mail-tracking-callback-procs.tcl
+++ tcl/mail-tracking-callback-procs.tcl
@@ -10,72 +10,89 @@ ad_library {
     @cvs-id $Id$
 }
 
-ad_proc -public -callback acs_mail_lite::complex_send -impl mail_tracking {
-    {-package_id:required}
-    {-from_party_id:required}
-    {-from_addr ""}
-    {-to_party_ids ""}
-    {-cc_party_ids ""}
-    {-bcc_party_ids ""}
-    {-to_addr ""}
-    {-cc_addr ""}
-    {-bcc_addr ""}
-    {-body ""}
-    {-message_id:required}
-    {-subject ""}
-    {-object_id ""}
-    {-file_ids ""}
-} {
-    create a new entry in the mail tracking table
-} {
-    # We need to put lindex here since the value from
-    # the swithc converts this "element element" to this
-    # "{element element}"
-    set file_ids [string trim $file_ids "{}"]
 
-    foreach optional_param {cc_party_ids bcc_party_ids to_addr cc_addr bcc_addr body subject object_id file_ids to_party_ids} {
-	if {![info exists $optional_param]} {
-	    set $optional_param {}
-	}
-    }
+# fraber 140906: Unable to fix error:
+# This callback produces strange errors that "from_party_id" is
+# not defined. Now just disabling, because mail-tracking doesn't
+# seem to be used anymore.
+#
+# ad_proc -public -callback acs_mail_lite::complex_send -impl mail_tracking {
+#     {-package_id:required}
+#     {-from_party_id:required}
+#     {-from_addr ""}
+#     {-to_party_ids ""}
+#     {-cc_party_ids ""}
+#     {-bcc_party_ids ""}
+#     {-to_addr ""}
+#     {-cc_addr ""}
+#     {-bcc_addr ""}
+#     {-body ""}
+#     {-message_id:required}
+#     {-subject ""}
+#     {-object_id ""}
+#     {-file_ids ""}
+# } {
+#     create a new entry in the mail tracking table
+# } {
+#     # We need to put lindex here since the value from
+#     # the swithc converts this "element element" to this
+#     # "{element element}"
+#     set file_ids [string trim $file_ids "{}"]
+# 
+#     foreach optional_param {cc_party_ids bcc_party_ids to_addr cc_addr bcc_addr body subject object_id file_ids to_party_ids} {
+# 	if {![info exists $optional_param]} {
+# 	    set $optional_param {}
+# 	}
+#     }
+# 
+#     set log_id [mail_tracking::new -package_id $package_id \
+# 		    -sender_id $from_party_id \
+# 		    -from_addr $from_addr \
+# 		    -recipient_ids $to_party_ids \
+# 		    -cc_ids $cc_party_ids \
+# 		    -bcc_ids $bcc_party_ids \
+# 		    -to_addr $to_addr \
+# 		    -cc_addr $cc_addr \
+# 		    -bcc_addr $bcc_addr \
+# 		    -body $body \
+# 		    -message_id $message_id \
+# 		    -subject $subject \
+# 		    -file_ids $file_ids \
+# 		    -object_id $object_id]
+# 
+# }
 
-    set log_id [mail_tracking::new -package_id $package_id \
-		    -sender_id $from_party_id \
-		    -from_addr $from_addr \
-		    -recipient_ids $to_party_ids \
-		    -cc_ids $cc_party_ids \
-		    -bcc_ids $bcc_party_ids \
-		    -to_addr $to_addr \
-		    -cc_addr $cc_addr \
-		    -bcc_addr $bcc_addr \
-		    -body $body \
-		    -message_id $message_id \
-		    -subject $subject \
-		    -file_ids $file_ids \
-		    -object_id $object_id]
 
-}
+# fraber 140906: Unable to fix error:
+# This callback produces strange errors that "from_party_id" is
+# not defined. Now just disabling, because mail-tracking doesn't
+# seem to be used anymore.
+#
+# ad_proc -public -callback acs_mail_lite::send -impl mail_tracking {
+#     {-package_id:required}
+#     {-from_party_id:required}
+#     {-to_party_id:required}
+#     {-body:required}
+#     {-message_id:required}
+#     {-subject:required}
+# } {
+#     create a new entry in the mail tracking table
+# } {
+#     if {[info exists from_party_id]} { return }
+#     if {[info exists to_party_id]} { return }
+# 
+#     ns_log Notice "acs_mail_lite::send -impl mail_tracking -package_id $package_id -from_party_id $from_party_id -to_party_id $to_party_id -message_id $message_id -subject $subject"
+# 
+#     set log_id [mail_tracking::new -package_id $package_id \
+# 		    -sender_id $from_party_id \
+# 		    -recipient_ids $to_party_id \
+# 		    -body $body \
+# 		    -message_id $message_id \
+# 		    -subject $subject]
+# 
+# }
 
-ad_proc -public -callback acs_mail_lite::send -impl mail_tracking {
-    {-package_id:required}
-    {-from_party_id:required}
-    {-to_party_id:required}
-    {-body:required}
-    {-message_id:required}
-    {-subject:required}
-} {
-    create a new entry in the mail tracking table
-} {
-    ns_log Notice "acs_mail_lite::send -impl mail_tracking -package_id $package_id -from_party_id $from_party_id -to_party_id $to_party_id -message_id $message_id -subject $subject"
 
-    set log_id [mail_tracking::new -package_id $package_id \
-		    -sender_id $from_party_id \
-		    -recipient_ids $to_party_id \
-		    -body $body \
-		    -message_id $message_id \
-		    -subject $subject]
-
-}
 
 ad_proc -public -callback fs::file_delete -impl mail-tracking {
     {-package_id:required}
